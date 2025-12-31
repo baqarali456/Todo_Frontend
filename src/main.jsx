3d8f572480935d2e175f0049be2e763e361030fa
baqar <sayedalintl@gmail.com>
2025-12-31 22:18:41 +0530

    fix error EditTodo is not defined in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
 import SignUp from './pages/SignUp.jsx'
 import TodoForm from './components/TodoForm.jsx'
+import EditTodo from './pages/EditTodo.jsx'
 
 const router = createBrowserRouter([
   {
